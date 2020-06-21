57cb5e2922e580058ed4925b6d9f4415118ddafe
szczepaniakmichal <mssk87@gmail.com>
2020-06-21 13:16:13 +0200

    3-iterators, 3-safe

--- src/3-iterators/3-safe.js
+++ src/3-iterators/3-safe.js
@@ -2,4 +2,27 @@
  * Cracks open the safe by yielding the correct values.
  */
 export function * open () {
+    const number = yield 'hello'
+    let name;
+    try {
+        yield  number * 42
+    } catch (e) {
+        name = e;
+    }
+    yield undefined
+    return name.toUpperCase();
 }
+
+
+
+// export function * open () {
+//     const number = yield 'hello'
+//     let name
+//     try {
+//         yield number * 42
+//     } catch (e) {
+//         name = e
+//     }
+//     yield undefined
+//     return name.toUpperCase()
+// }
